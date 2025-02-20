11ed276b2f19166e0931040925407f31f7f8d2da
Poquet <jose.martinez@ukio.com>
2025-02-20 16:12:24 +0100

    Avoid magic strings, move logic for sulfuras and sell in

--- src/application/gilded_rose.py
+++ src/application/gilded_rose.py
@@ -2,6 +2,12 @@ from typing import List
 
 from domain.item import Item
 
+BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+
+AGED_BRIE = "Aged Brie"
+
+SULFURAS = "Sulfuras, Hand of Ragnaros"
+
 
 class GildedRose(object):
 
@@ -10,31 +16,30 @@ class GildedRose(object):
 
     def update_quality(self) -> None:
         for item in self.items:
-            if (
-                item.name != "Aged Brie"
-                and item.name != "Backstage passes to a TAFKAL80ETC concert"
-            ):
+            if item.name == SULFURAS:
+                continue
+
+            item.sell_in = self.__decrease_sell_in(item)
+
+            if item.name != AGED_BRIE and item.name != BACKSTAGE_PASS:
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = self.__reduce_quality(item)
+                    item.quality = self.__reduce_quality(item)
             else:
                 if item.quality < 50:
                     item.quality = self.__increase_quality(item)
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name == BACKSTAGE_PASS:
                         if item.sell_in < 11:
                             if item.quality < 50:
                                 item.quality = self.__increase_quality(item)
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = self.__increase_quality(item)
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = self.__decrease_sell_in(item)
+
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.name != AGED_BRIE:
+                    if item.name != BACKSTAGE_PASS:
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = self.__reduce_quality(item)
+                            item.quality = self.__reduce_quality(item)
                     else:
                         item.quality = item.quality - item.quality
                 else:
