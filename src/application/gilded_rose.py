2f2effe7dd6c5358d99d3e8c581ff3d7f0888a9e
Poquet <jose.martinez@ukio.com>
2025-02-20 15:54:29 +0100

    Extract to method item modifications

--- src/application/gilded_rose.py
+++ src/application/gilded_rose.py
@@ -16,27 +16,36 @@ class GildedRose(object):
             ):
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                        item.quality = self.__reduce_quality(item)
             else:
                 if item.quality < 50:
-                    item.quality = item.quality + 1
+                    item.quality = self.__increase_quality(item)
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_in < 11:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                item.quality = self.__increase_quality(item)
                         if item.sell_in < 6:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                item.quality = self.__increase_quality(item)
             if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+                item.sell_in = self.__decrease_sell_in(item)
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                                item.quality = self.__reduce_quality(item)
                     else:
                         item.quality = item.quality - item.quality
                 else:
                     if item.quality < 50:
-                        item.quality = item.quality + 1
+                        item.quality = self.__increase_quality(item)
+
+    def __decrease_sell_in(self, item: Item) -> int:
+        return int(item.sell_in - 1)
+
+    def __increase_quality(self, item: Item) -> int:
+        return int(item.quality + 1)
+
+    def __reduce_quality(self, item: Item) -> int:
+        return int(item.quality - 1)
