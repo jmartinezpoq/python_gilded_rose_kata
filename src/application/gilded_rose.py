2888028a61aebb2b4ca7722168b8b3bee1b72940
Poquet <jose.martinez@ukio.com>
2025-02-20 16:20:34 +0100

    Separate logic for backstage pass, aged brie and normal item

--- src/application/gilded_rose.py
+++ src/application/gilded_rose.py
@@ -8,6 +8,8 @@ AGED_BRIE = "Aged Brie"
 
 SULFURAS = "Sulfuras, Hand of Ragnaros"
 
+MAX_QUALITY = 50
+
 
 class GildedRose(object):
 
@@ -21,36 +23,57 @@ class GildedRose(object):
 
             item.sell_in = self.__decrease_sell_in(item)
 
-            if item.name != AGED_BRIE and item.name != BACKSTAGE_PASS:
-                if item.quality > 0:
-                    item.quality = self.__reduce_quality(item)
-            else:
-                if item.quality < 50:
-                    item.quality = self.__increase_quality(item)
-                    if item.name == BACKSTAGE_PASS:
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = self.__increase_quality(item)
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = self.__increase_quality(item)
+            if item.name == BACKSTAGE_PASS:
+                item.quality = self.__increase_quality_for_backstage_pass(item)
+                continue
 
-            if item.sell_in < 0:
-                if item.name != AGED_BRIE:
-                    if item.name != BACKSTAGE_PASS:
-                        if item.quality > 0:
-                            item.quality = self.__reduce_quality(item)
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = self.__increase_quality(item)
+            if item.name == AGED_BRIE:
+                item.quality = self.__increase_quality_for_aged_brie(item)
+                continue
+
+            if item.quality > 0:
+                item.quality = self.__reduce_quality_for_normal_item(item)
 
     def __decrease_sell_in(self, item: Item) -> int:
         return int(item.sell_in - 1)
 
-    def __increase_quality(self, item: Item) -> int:
-        return int(item.quality + 1)
+    def __calculate_multiplier_for_normal_item(self, item: Item) -> int:
+        multiplier = 1
+        if item.sell_in < 0:
+            multiplier = 2
 
-    def __reduce_quality(self, item: Item) -> int:
-        return int(item.quality - 1)
+        return multiplier
+
+    def __calculate_multiplier_for_backstage_pass(self, item: Item) -> int:
+        if item.sell_in < 0:
+            return 0
+        if item.sell_in < 5:
+            return 3
+        if item.sell_in < 10:
+            return 2
+
+        return 1
+
+    def __increase_quality_for_aged_brie(self, item: Item) -> int:
+        if item.quality < MAX_QUALITY:
+            return int(item.quality + 1)
+
+        return int(item.quality)
+
+    def __increase_quality_for_backstage_pass(self, item: Item) -> int:
+        if item.sell_in < 0:
+            return 0
+
+        multiplier = self.__calculate_multiplier_for_backstage_pass(item)
+        if item.quality < MAX_QUALITY:
+            return int(item.quality + 1 * multiplier)
+
+        return int(item.quality)
+
+    def __reduce_quality_for_normal_item(self, item: Item) -> int:
+        multiplier = self.__calculate_multiplier_for_normal_item(item)
+
+        if item.quality > 0:
+            return int(item.quality - 1 * multiplier)
+
+        return int(item.quality)
